eed0eb80212e6b4347e966dd2ada6babca68f52e
vviktorovich <viktor.viktor.mir@gmail.com>
2020-01-13 11:10:43 +0200

    Removed all dependecies

--- loadtests/locust_start.bat
+++
@@ -1,7 +0,0 @@
-start /b locust -f auth.py --master
-start /b locust -f auth.py --slave
-start /b locust -f auth.py --slave
-start /b locust -f auth.py --slave
-start /b locust -f auth.py --slave
-start /b locust -f auth.py --slave
-start /b locust -f auth.py --slave
--- loadtests/start_test.bat
+++
@@ -1,1 +0,0 @@
-pipenv run locust_start.bat
